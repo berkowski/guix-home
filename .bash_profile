188702377ca11c108f2d6c21bf1afdaf873914f3
Zac Berkowitz <zac.berkowitz@gmail.com>
2022-06-14 15:33:37 -0400

    Initial commit

---
+++ .bash_profile
@@ -0,0 +1,7 @@
+export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
+export XCURSOR_PATH="$HOME/.local/share/icons:$HOME/.icons:/usr/share/icons:/usr/share/pixmaps"
+
+export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
+# GUIX_PROFILE="${HOME}/.config/guix/current"
+# . "$GUIX_PROFILE/etc/profile"
---
+++ config.scm
@@ -0,0 +1,77 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of your profile.  This is "symbolic": it only
+;; specifies package names.  To reproduce the exact same profile, you also
+;; need to capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(use-modules
+ (gnu home)
+ (gnu packages)
+ (gnu packages emacs)
+ (gnu services)
+ (guix gexp)
+ (gnu home services)
+ (gnu home services xdg)
+ (gnu home services shells)
+ (gnu home services shepherd))
+
+(home-environment
+ (packages
+  (map (compose list specification->package+output)
+       (list "taskwarrior"
+             "emacs"
+             "emacs-all-the-icons"
+             "emacs-dockerfile-mode"
+             "emacs-doom-modeline"
+             "emacs-evil"
+             "emacs-evil-collection"
+             "emacs-flycheck"
+             "emacs-helpful"
+             "emacs-lsp-mode"
+             "emacs-lsp-ui"
+             "emacs-magit"
+             "emacs-mu4e-alert"
+             "emacs-projectile"
+             "emacs-no-littering"
+             "emacs-rustic"
+             "emacs-use-package"
+             "emacs-which-key"
+             "emacs-selectrum"
+             "emacs-undo-fu"
+             "emacs-org"
+             "emacs-org-superstar"
+             "emacs-yasnippet"
+             "emacs-company"
+             "emacs-toml-mode"
+             "font-fira-code"
+             "font-abattis-cantarell"
+             "git"
+             "glibc-locales"
+	     "mandoc"
+             "nss-certs"
+             "picocom"
+             "rust-analyzer"
+             "ripgrep"
+					;"timewarrior"
+             "tmux")))
+ (services
+  (list
+   (service home-bash-service-type
+	    (home-bash-configuration
+	     (bash-profile
+	      (list (local-file "./.bash_profile" "bash_profile")))))
+   (service home-shepherd-service-type
+	    (home-shepherd-configuration
+	     (services (list
+			(shepherd-service
+			 (provision '(emacs))
+			 (start #~(make-forkexec-constructor
+				   (list
+				    #$(file-append emacs "/bin/emacs")
+				    "--fg-daemon")))
+			 (stop #~(make-system-destructor
+				  (string-join 
+				   (list
+				    (string-append #$emacs "/bin/emacsclient")
+				    "--eval"
+				    "\"(kill-emacs)\""))))))))))))
