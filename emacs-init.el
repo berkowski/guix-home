7088da9035d2d499481b5841486c4045845ab99a
Zac Berkowitz <zac.berkowitz@gmail.com>
2022-06-16 22:22:09 -0400

    Add completion framework

--- emacs-init.el
+++ emacs-init.el
@@ -108,6 +108,80 @@
 (require 'magit)
 (global-set-key (kbd "C-c g") 'magit-file-dispatch)  ; the recommended keybinding
 
+;;; Completion and Narrowing
+
+;; Vertico
+;;
+;; Vertico - a fast, minimal, vertical completion UI
+;; guix dependencies:
+;; - emacs-vertico
+(require 'vertico)
+(require 'vertico-directory)
+
+(with-eval-after-load 'evil
+  (define-key vertico-map (kbd "C-j") 'vertico-next)
+  (define-key vertico-map (kbd "C-k") 'vertico-previous)
+  (define-key vertico-map (kbd "M-h") 'vertico-directory-up))
+
+(customize-set-variable 'vertico-cycle t) ; Cycle back to top/bottom result when the edge is reached
+(vertico-mode 1) ; Start Vertico
+
+;; Marginalia
+;;
+;; Marginalia - annotations for completion buffers
+;; guix dependencies:
+;; - emacs-marginalia
+(require 'marginalia)
+(customize-set-variable 'marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+(marginalia-mode 1)
+
+;; Orderless
+;; 
+;; Orderless - better fuzzy matching
+;; guix dependencies:
+;; - emacs-orderless
+(require 'orderless)
+(customize-set-variable 'completion-styles '(orderless))
+(customize-set-variable 'completion-category-overrides '((file (styles . (partial-completion)))))
+
+;; Embark
+;;
+;; Embark - context-based actions using keymaps
+;; guix dependencies:
+;; - emacs-embark
+(require 'embark)
+(require 'embark-consult)
+
+(global-set-key [remap describe-bindings] #'embark-bindings)
+(global-set-key (kbd "C-.") 'embark-act)
+
+;; Use Embark to show bindings in a key prefix with `C-h`
+(setq prefix-help-command #'embark-prefix-help-command)
+
+(with-eval-after-load 'embark-consult
+  (add-hook 'embark-collect-mode-hook #'consult-preview-at-point-mode))
+
+;; Corfu
+;;
+;; Corfu - completion overlay
+;; guix dependencies:
+;; - emacs-corfu
+;; - emacs-corfu-doc
+(require 'corfu)
+(require 'corfu-doc)
+(customize-set-variable 'corfu-cycle t) ; Allows cycling through candidates
+(customize-set-variable 'corfu-auto t)  ; Enable auto completion
+(customize-set-variable 'corfu-auto-prefix 2) ; Complete with less prefix keys
+(customize-set-variable 'corfu-auto-delay 0.0) ; No delay for completion
+(customize-set-variable 'corfu-echo-documentation 0.25) ; Echo docs for current completion option
+
+(global-corfu-mode 1)
+
+(add-hook 'corfu-mode-hook #'corfu-doc-mode)
+(define-key corfu-map (kbd "M-p") #'corfu-doc-scroll-down)
+(define-key corfu-map (kbd "M-n") #'corfu-doc-scroll-up)
+(define-key corfu-map (kbd "M-d") #'corfu-doc-toggle)
+
 ;;; Software development
 
 ;; Rust
